e7a4b93bcd64eddaebffbcd51b2aebf89f91da06
exZon exZon <yashpatel5566789>
2020-08-23 11:39:40 +0530

    final complete

---
+++ src/keys.js
@@ -0,0 +1,3 @@
+export const API_KEY = "AIzaSyDT30ik5nEH9d-Vck4Fj3rgNgFlf0F7m9s"
+
+export default API_KEY;
---
+++ src/reducer.js
@@ -0,0 +1,27 @@
+  
+export const initialState = {
+    term: null,
+  };
+
+  export const actionType = {
+      SET_SEARCH_TERM: "SET_SEARCH_TERM",
+  }
+  
+  const reducer = (state, action) => {
+    console.log(action);
+  
+    // action -> type, [payload]
+  
+    switch (action.type) {
+      case "SET_SEARCH_TERM":
+        return {
+          ...state,
+          term: action.term,
+        };
+  
+      default:
+        return state;
+    }
+  };
+  
+  export default reducer;
